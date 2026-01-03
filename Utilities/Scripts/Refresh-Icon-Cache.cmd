b498e58a463c75df89817d7bf678a3e85fac014c
OG Nimbi <darkwater409@gmail.com>
2026-01-02 22:11:08 -0700

    Improve Refresh Icon Cache script layout and error handling

--- Utilities/Scripts/Refresh-Icon-Cache.cmd
+++ Utilities/Scripts/Refresh-Icon-Cache.cmd
@@ -5,7 +5,7 @@ SET ICONCACHE_X="%LOCALAPPDATA%\Microsoft\Windows\Explorer\iconcache*"
 SET THUMBCACHE="%LOCALAPPDATA%\Microsoft\Windows\Explorer\thumbcache*"
 
 CLS
-MODE CON COLS=45 LINES=15
+MODE CON COLS=52 LINES=15
 COLOR 97
 TITLE Refresh Icon Cache
 ECHO ::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -20,55 +20,73 @@ GOTO REFRESH_ICON_CACHE
 
 :REFRESH_ICON_CACHE
 CLS
-MODE CON COLS=45 LINES=25
+MODE CON COLS=52 LINES=15
 COLOR 1F
 TITLE Applying
 ECHO :::::::::::::::::::::::::::::::::::::::::::::
-ECHO ::          Refreshing Icon Cache          ::
+ECHO ::              Refresh Icon Cache                ::
 ECHO :::::::::::::::::::::::::::::::::::::::::::::
 ECHO.
-ECHO Please wait...
+ECHO Refreshing Icon Cache... Please wait.
 ECHO.
 
-TASKKILL /F /IM explorer.exe >NUL
-DEL /A /F /Q %ICONCACHE% >NUL
-DEL /A /F /Q %ICONCACHE_X% >NUL
-DEL /A /F /Q %THUMBCACHE% >NUL
-TIMEOUT /T 3 >NUL
-START explorer.exe >NUL
-IF %ERRORLEVEL% EQU 0 (
-	GOTO SUCCESS
-) ELSE (
-	GOTO FAILED
-)
+IF EXIST %ICONCACHE% ( 
+	DEL /A /F /Q %ICONCACHE% >NUL
+	TIMEOUT /T 1 >NUL
+	 ) ELSE ( 
+		ECHO Icon cache file not found, skipping...
+		TIMEOUT /T 1 >NUL
+	 ) 
+IF EXIST %ICONCACHE_X% ( 
+	DEL /A /F /Q %ICONCACHE_X% >NUL
+	TIMEOUT /T 1 >NUL
+	) ELSE ( 
+		ECHO Icon cache files not found, skipping...
+		TIMEOUT /T 1 >NUL
+		)
+
+IF EXIST %THUMBCACHE% ( 
+	DEL /A /F /Q %THUMBCACHE% >NUL 
+	TIMEOUT /T 1 >NUL
+	) ELSE ( 
+		ECHO Thumbnail cache files not found, skipping...
+		TIMEOUT /T 1 >NUL
+		)
+
+IF %ERRORLEVEL%==0 GOTO SUCCESS ELSE GOTO ERROR
 
 :SUCCESS
-	CLS
-	MODE CON COLS=45 LINES=15
-	COLOR 2F
-	TITLE Success
-	ECHO :::::::::::::::::::::::::::::::::::::::::::::
-	ECHO ::                Success                  ::
-	ECHO :::::::::::::::::::::::::::::::::::::::::::::
-	ECHO.
-	ECHO Icon cache has been successfully refreshed.
-	GOTO EXIT
+CLS
+MODE CON COLS=52 LINES=15
+COLOR 2F
+TITLE Success
+ECHO :::::::::::::::::::::::::::::::::::::::::::::
+ECHO ::                Success                  ::
+ECHO :::::::::::::::::::::::::::::::::::::::::::::
+ECHO.
+ECHO Icon cache has been successfully refreshed.
+ECHO.
+ECHO Explorer will now restart to apply changes.
+TASKKILL /F /IM explorer.exe >NUL
+TIMEOUT /T 1 >NUL
+START explorer.exe >NUL
+GOTO EXIT
 
-:FAILED
-	CLS
-	MODE CON COLS=45 LINES=15
-	COLOR 4F
-	TITLE Error: Command Failed
-	ECHO :::::::::::::::::::::::::::::::::::::::::::::
-	ECHO ::            Command Failed               ::
-	ECHO :::::::::::::::::::::::::::::::::::::::::::::
-	ECHO.
-	ECHO An error occurred while refreshing the icon cache.
-	GOTO EXIT
+:ERROR
+CLS
+MODE CON COLS=52 LINES=15
+COLOR 4F
+TITLE Error: Command Failed
+ECHO :::::::::::::::::::::::::::::::::::::::::::::
+ECHO ::            Command Failed               ::
+ECHO :::::::::::::::::::::::::::::::::::::::::::::
+ECHO.
+ECHO An error occurred while refreshing the icon cache.
+ECHO Please try running this script again.
+GOTO EXIT
 
 :EXIT
-	ECHO.
-	ECHO You can now close this window. It will close automatically in 5 seconds.
-	TIMEOUT /T 5 >NUL
-	EXIT
-
+ECHO.
+ECHO You can now close this window. It will close automatically in 5 seconds.
+TIMEOUT /T 5 >NUL
+EXIT
